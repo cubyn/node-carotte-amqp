a1b933a4871bc025381878a765e92b07a8e6625c
Jonathan Muller <jonathan@cubyn.com>
2017-02-08 14:03:10 +0100

    Refactor tests to wait for queue to be created

--- tests/rpc.spec.js
+++ tests/rpc.spec.js
@@ -3,31 +3,33 @@ const carotte = require('../src')();
 
 describe('rpc', () => {
     it('should be able to receive a response from a queue', () => {
-        carotte.subscribe('direct/hello-rpc', () => {
+        return carotte.subscribe('direct/hello-rpc', { queue: { exclusive: true } }, () => {
             return { a: 1 };
+        })
+        .then(() => {
+            return carotte.invoke('direct/hello-rpc', {}, {})
+                .then(({ data }) => {
+                    expect(data).to.be.defined;
+                    expect(data.a).to.be.eql(1);
+                });
         });
-
-        return carotte.invoke('direct/hello-rpc', {}, {})
-            .then(({ data }) => {
-                expect(data).to.be.defined;
-                expect(data.a).to.be.eql(1);
-            });
     });
 
     it('should receive response on data object', () => {
-        carotte.subscribe('direct/hello-rpc2', ( { data }) => {
+        carotte.subscribe('direct/hello-rpc2', { queue: { exclusive: true } }, ( { data }) => {
             return { a: 2 };
+        })
+        .then(() => {
+            return carotte.invoke('direct/hello-rpc2', {})
+                .then(({ data }) => {
+                    expect(data).to.be.defined;
+                    expect(data.a).to.be.eql(2);
+                });
         });
-
-        return carotte.invoke('direct/hello-rpc2', {})
-            .then(({ data }) => {
-                expect(data).to.be.defined;
-                expect(data.a).to.be.eql(2);
-            });
     });
 
     it('should not handle RPC response if consumer respond with bad correlation ID', (done) => {
-        carotte.subscribe('direct/hello-rpc3', ( { data, headers }) => {
+        carotte.subscribe('direct/hello-rpc3', { queue: { exclusive: true } }, ( { data, headers }) => {
             try {
                 headers['x-correlation-id'] = 'toto';
                 setTimeout(done, 1000);
@@ -35,11 +37,12 @@ describe('rpc', () => {
             } catch(err) {
                 done(err);
             }
+        })
+        .then(() => {
+            carotte.invoke('direct/hello-rpc3', {})
+                .then(({ data }) => {
+                    done(new Error('Should not execute callback'));
+                });
         });
-
-        carotte.invoke('direct/hello-rpc3', {})
-            .then(({ data }) => {
-                throw new Error('Should not execute callback');
-            });
     });
 });
--- tests/subscriber.spec.js
+++ tests/subscriber.spec.js
@@ -5,47 +5,37 @@ describe('subscriber', () => {
 
     describe('direct', () => {
 
-        it('should be able to receive a message on a queue (direct exchange)', done => {
+        it('should be able to receive a message on a queue (no option)', done => {
             carotte.subscribe('direct/hello', () => {
                 done();
-            });
-
-            carotte.publish('direct/hello', {});
+            })
+            .then(() => carotte.publish('direct/hello', {}));
         });
 
         it('should receive message on data object', done => {
-            carotte.subscribe('direct/hello1', ( { data }) => {
+            carotte.subscribe('direct/hello1', { queue: { exclusive: true } }, ( { data }) => {
                 expect(data).to.be.defined;
                 expect(data.hello).to.be.defined;
                 expect(data.hello).to.be.eql('world');
                 done();
-            });
-
-            carotte.publish('direct/hello1', { hello: 'world' });
+            })
+            .then(() => carotte.publish('direct/hello1', { hello: 'world' }));
         });
     });
 
     describe('fanout', () => {
 
-        it('should be able to send message on a fanout exchange', () => {
-            return carotte.publish('fanout/fanout-routing-key', {})
-                .then((res) => {
-                    expect(res).to.be.true;
-                });
-        });
-
         it('should be able to receive a message on a fanout exchange', (done) => {
-            carotte.subscribe('fanout/fanout-key-1/queue-name', ( { data } ) => {
+            carotte.subscribe('fanout/queue-name', { queue: { exclusive: true } }, ( { data } ) => {
                 try {
                     expect(data.hello).to.be.defined;
                     expect(data.hello).to.be.eql('world');
-                    //done();
+                    done();
                 } catch(err) {
                     done(err);
                 }
-            });
-
-            carotte.publish('fanout/fanout-key-1', { hello: 'world' });
+            })
+            .then(() => carotte.publish('fanout', { hello: 'world' }));
         });
     });
 
@@ -59,17 +49,16 @@ describe('subscriber', () => {
         });
 
         it('should be able to receive a message on a topic exchange', (done) => {
-            carotte.subscribe('topic/topic-key-1/queue-name', ( { data } ) => {
+            carotte.subscribe('topic/topic-key-1/queue-name', { queue: { exclusive: true } }, ( { data } ) => {
                 try {
                     expect(data.hello).to.be.defined;
                     expect(data.hello).to.be.eql('world');
-                    //done();
+                    done();
                 } catch (err) {
                     done(err);
                 }
-            });
-
-            carotte.publish('topic/topic-key-1', { hello: 'world' });
+            })
+            .then(() => carotte.publish('topic/topic-key-1', { hello: 'world' }));
         });
     });
 
@@ -77,13 +66,12 @@ describe('subscriber', () => {
 
         it('should republish a message if subscriber fails', (done) => {
             let callCount = 0;
-            carotte.subscribe('direct/republish', ( { data }) => {
+            carotte.subscribe('direct/republish', { queue: { exclusive: true } }, ( { data }) => {
                 if (callCount === 1) return done();
                 callCount++;
                 throw new Error('An error occured');
-            });
-
-            carotte.publish('direct/republish', { hello: 'world' });
+            })
+            .then(() => carotte.publish('direct/republish', { hello: 'world' }));
         });
     });
 });
