7b840eb07f2fe3609c80ef623b6a455bf69f33d2
Jonathan Muller <jonathan@cubyn.com>
2017-02-10 17:22:37 +0100

    Document code and reset stats once sent

--- src/bouillon-agent.js
+++ src/bouillon-agent.js
@@ -12,6 +12,10 @@ const responseHandler = {
 
 const subscribers = {};
 
+/**
+ * Get service informations, with all stored subscribers with their associated meta and data
+ * @return {object} Object containing service data and maintainers/subscribers
+ */
 function getAll() {
     return {
         name: serviceName,
@@ -21,6 +25,14 @@ function getAll() {
     };
 }
 
+/**
+ * Add a subscriber to the subscriber pool, and init its stats
+ * @param {string} qualifier the carotte qualifier (see doc)
+ * @param {object} subscriber subscriber metas
+ * @param {object.requestSchema} requestSchema JSONSchema for subscriber input parameters
+ * @param {object.responseSchema} responseSchema JSONSchema for subscriber response
+ * @param {object.version} [version] the subscriber version code
+ */
 function addSubscriber(qualifier, subscriber) {
     subscriber.performances = {
         duration: {
@@ -42,6 +54,13 @@ function addSubscriber(qualifier, subscriber) {
     subscribers[qualifier] = subscriber;
 }
 
+/**
+ * Register an usage stat into the service description
+ * @param  {string} qualifier The carotte qualifier (see doc)
+ * @param  {number} duration  The subscriber function processing time
+ * @param  {string} caller    The caller service name
+ * @return {void}
+ */
 function logStats(qualifier, duration, caller) {
     const subscriber = subscribers[qualifier];
     if (subscriber) {
@@ -68,13 +87,28 @@ function logStats(qualifier, duration, caller) {
     }
 }
 
+/**
+ * Enable the bouillon agent and register the listener fanout queue
+ * It will answer to all fanout messages using the responseHandler defined in this file
+ * @param  {object} carotte A carotte instance
+ * @return {Promise}        Resolves when bouillon listening queue is correctly registered
+ */
 function ensureBouillonAgent(carotte) {
+    // create a fanout subscriber to receive bouillon requests
     return carotte.subscribe('fanout', {
         exchangeName: 'bouillon.fanout',
         queue: { durable: false, exclusive: true }
     }, ({ data }) => {
         if (data) {
-            return responseHandler[data.origin][data.type]();
+            const serviceData = responseHandler[data.origin][data.type]();
+            const response = JSON.parse(JSON.stringify(serviceData));
+
+            // reset all stats until next broadcast
+            for (const subscriber in serviceData.subscribers) {
+                addSubscriber(subscriber, serviceData.subscribers[subscriber]);
+            }
+
+            return response;
         }
         return false;
     });
