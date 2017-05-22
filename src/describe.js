fae5bee2a7e42f7f59550dd527cea7eca025ddd2
Jonathan Muller <jonathan@cubyn.com>
2017-05-22 16:34:40 +0200

    Dont subscribe to describe on topic/fanout routes

--- src/describe.js
+++ src/describe.js
@@ -1,4 +1,7 @@
+const replaceNonDirect = /^(topic|fanout)\//;
+
 module.exports.subscribeToDescribe = function (carotte, qualifier, meta) {
+    qualifier = qualifier.replace(replaceNonDirect, 'direct/');
     carotte.subscribe(`${qualifier}:describe`, { queue: { durable: false, autoDelete: true } }, () => {
         return meta;
     });
