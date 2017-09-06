dac19e87d73dc93110d3a9cf2ee3f13d3d9b3e86
leolassence <leolassence@gmail.com>
2017-09-06 16:43:54 +0200

    Add stack transaction id array to context to improve stack trace

---
+++ tests/stack-transaction.spec.js
@@ -0,0 +1,129 @@
+const expect = require('chai').expect;
+const carotte = require('./client')({
+    autoDescribe: false
+});
+
+describe('Local transactionId', () => {
+    const options = { queue: { exclusive: true } };
+
+    it('should create a transactionStack when it\'s not defined', async () => {
+        await carotte.subscribe('stack-test-1-1', options, ({ context }) => {
+            expect(context).to.be.defined;
+            expect(context.transactionStack).to.be.defined;
+            expect(context.transactionStack).to.be.an('array');
+            expect(context.transactionStack).to.have.length(1);
+        });
+
+        await carotte.invoke('stack-test-1-1', { context: {} }, {});
+    });
+
+    it('should add new transactionStack id to transactionStack', async () => {
+        await carotte.subscribe('stack-test-2-1', {
+            queue: { exclusive: true }
+        }, ({ context }) => {
+            expect(context).to.be.defined;
+            expect(context.transactionStack).to.be.defined;
+            expect(context.transactionStack).to.be.an('array');
+            expect(context.transactionStack).to.have.length(4);
+        });
+
+        await carotte.invoke('stack-test-2-1', {
+            context: {
+                transactionStack: ['1234', '5678', '9012']
+            }
+        }, {});
+    });
+
+    it('should add transactionStack ids in cascade', async () => {
+        await carotte.subscribe('stack-test-3-1', options, ({ context, invoke }) => {
+            expect(context).to.be.defined;
+            expect(context.transactionStack).to.be.defined;
+            expect(context.transactionStack).to.be.an('array');
+            expect(context.transactionStack).to.have.length(1);
+            return invoke('stack-test-3-2', {});
+        });
+
+        await carotte.subscribe('stack-test-3-2', options, ({ context, invoke }) => {
+            expect(context.transactionStack).to.have.length(2);
+            return invoke('stack-test-3-3', {});
+        });
+
+        await carotte.subscribe('stack-test-3-3', options, ({ context, invoke }) => {
+            expect(context.transactionStack).to.have.length(3);
+            return invoke('stack-test-3-4', {});
+        });
+
+        await carotte.subscribe('stack-test-3-4', options, ({ context }) => {
+            expect(context.transactionStack).to.have.length(4);
+            return true;
+        });
+
+        return carotte.invoke('stack-test-3-1', {});
+    });
+
+    it('should only add stack transaction ids to there rights branchs', async () => {
+        let stackId1 = '';
+        let stackId4 = '';
+
+        await carotte.subscribe('stack-test-4-1', options, ({ context, invoke }) => {
+            stackId1 = context.transactionStack[0];
+            expect(context).to.be.defined;
+            expect(context.transactionStack).to.be.defined;
+            expect(context.transactionStack).to.be.an('array');
+            expect(context.transactionStack).to.have.length(1);
+
+            invoke('stack-test-4-2', {});
+            invoke('stack-test-4-3', {});
+            return invoke('stack-test-4-4', {});
+        });
+
+        await carotte.subscribe('stack-test-4-2', options, ({ context }) => {
+            expect(context['origin-consumer']).to.be.deep.eql('stack-test-4-1');
+            expect(context.transactionStack).to.have.length(2);
+            expect(context.transactionStack[0]).to.be.eql(stackId1);
+            return true;
+        });
+
+        await carotte.subscribe('stack-test-4-3', options, ({ context }) => {
+            expect(context['origin-consumer']).to.be.deep.eql('stack-test-4-1');
+            expect(context.transactionStack).to.have.length(2);
+            expect(context.transactionStack[0]).to.be.eql(stackId1);
+            return true;
+        });
+
+        await carotte.subscribe('stack-test-4-4', options, ({ context, invoke }) => {
+            stackId4 = context.transactionStack[1];
+            expect(context.transactionStack).to.have.length(2);
+            expect(context.transactionStack[0]).to.be.eql(stackId1);
+            invoke('stack-test-4-5', {});
+            invoke('stack-test-4-6', {});
+            return invoke('stack-test-4-7', {});
+        });
+
+        await carotte.subscribe('stack-test-4-5', options, ({ context }) => {
+            expect(context['origin-consumer']).to.be.deep.eql('stack-test-4-4');
+            expect(context.transactionStack).to.have.length(3);
+            expect(context.transactionStack[0]).to.be.eql(stackId1);
+            expect(context.transactionStack[1]).to.be.eql(stackId4);
+            return true;
+        });
+
+        await carotte.subscribe('stack-test-4-6', options, ({ context }) => {
+            expect(context['origin-consumer']).to.be.deep.eql('stack-test-4-4');
+            expect(context.transactionStack).to.have.length(3);
+            expect(context.transactionStack[0]).to.be.eql(stackId1);
+            expect(context.transactionStack[1]).to.be.eql(stackId4);
+            return true;
+        });
+
+        await carotte.subscribe('stack-test-4-7', options, ({ context }) => {
+            expect(context['origin-consumer']).to.be.deep.eql('stack-test-4-4');
+            expect(context.transactionStack).to.have.length(3);
+            expect(context.transactionStack[0]).to.be.eql(stackId1);
+            expect(context.transactionStack[1]).to.be.eql(stackId4);
+            return true;
+        });
+
+        return carotte.invoke('stack-test-4-1', {});
+    });
+});
